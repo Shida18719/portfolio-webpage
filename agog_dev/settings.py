fd4c0504013003dd695ee5cf8683e4282a11f741
Rashidat <rawsheedah@yahoo.com>
2025-07-07 12:46:11 +0100

    Modify for deployement error

--- agog_dev/settings.py
+++ agog_dev/settings.py
@@ -187,16 +187,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_URL = '/static/'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME' : os.getenv('CLOUDINARY_CLOUD_NAME'), 
@@ -205,7 +205,7 @@ CLOUDINARY_STORAGE = {
 }
 
 WHITENOISE_MANIFEST_STRICT = False
-# WHITENOISE_SKIP_MISSING_FILES = True
+WHITENOISE_SKIP_MISSING_FILES = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

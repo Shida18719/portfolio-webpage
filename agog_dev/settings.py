b1024c898c19d64d8e89938e5b399e3bd30b16ba
Rashidat <rawsheedah@yahoo.com>
2025-07-07 15:53:18 +0100

    Revert to simpler compress in whitenoise

--- agog_dev/settings.py
+++ agog_dev/settings.py
@@ -193,7 +193,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
     
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 

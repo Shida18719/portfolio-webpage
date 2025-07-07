04100eb81f3a0434f0ad187d3a12b4f6b6ef6442
Rashidat <rawsheedah@yahoo.com>
2025-07-07 17:40:53 +0100

    Remove verbosity and compression

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input --clear --verbosity=2
+python manage.py collectstatic --no-input --clear
 
 python manage.py migrate

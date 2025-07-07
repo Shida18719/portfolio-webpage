b377face20709cf4fac7a7d5be3620890fd14190
Rashidat <rawsheedah@yahoo.com>
2025-07-07 12:27:46 +0100

    Add manifest strict to fix deployement error

--- agog_dev/settings.py
+++ agog_dev/settings.py
@@ -204,7 +204,8 @@ CLOUDINARY_STORAGE = {
     'API_SECRET' : os.getenv('CLOUDINARY_API_SECRET'),
 }
 
-WHITENOISE_SKIP_MISSING_FILES = True
+WHITENOISE_MANIFEST_STRICT = False
+# WHITENOISE_SKIP_MISSING_FILES = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
